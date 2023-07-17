256cb6a902e6300fe5e4dfe6a23d4910d245d541
EricWebDev <erikasj1999@gmail.com>
2023-07-17 19:50:34 +0300

    10

--- test.py
+++ test.py
@@ -7,8 +7,16 @@ sw, sh = 800, 600
 screen = pygame.display.set_mode((sw, sh))
 pygame.display.set_caption('Bybis')
 
+
 # Load the image
-bg = pygame.image.load('asteroidsPics/starbg.png')
+bg = pygame.image.load('asteroidsPics\starbg.png')
+alienImg = pygame.image.load('asteroidsPics/alienShip.png')
+playerRocket = pygame.image.load('asteroidsPics/spaceRocket.png')
+star = pygame.image.load('asteroidsPics/star.png')
+asteroid50 = pygame.image.load('asteroidsPics/asteroid50.png')
+asteroid100 = pygame.image.load('asteroidsPics/asteroid100.png')
+asteroid150 = pygame.image.load('asteroidsPics/asteroid150.png')
+
 
 # Main game loop
 running = True
